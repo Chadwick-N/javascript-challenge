b1c3d1ffa781465e378dbe0be019f6567893d07a
Max Nguyen <chadwick.uxd@gmail.com>
2021-04-16 22:11:20 -0400

    app update

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,7 +1,29 @@
 // from data.js
-var tableData = data;
-var tableColumns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"];
-var tableBody = d3.select("tbody");
+var data = data;
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"];
+var body = d3.select("tbody");
 var searchDate = d3.select("#datetime");
 var button = d3.select("#filter-btn");
 
+var loadTable = (ufoData) => {
+    body.html("");
+    ufoData.forEach(sightings => {
+        var tableRow = body.append("tr");
+        columns.forEach(column => tableRow.append("td").text(sightings[column]))
+    });
+}
+
+loadTable(data);
+
+button.on("click", () => {
+    d3.event.preventDefault();
+    userDate = searchDate.property("value");
+    var filterDate = data.filter(data => data.datetime === userDate);
+    body.html("");
+    if(filterDate.length !== 0) {
+        loadTable(filterDate);
+    }
+        else {
+            body.append("tr").append("td").text("Unavailable Date");
+        }
+});
