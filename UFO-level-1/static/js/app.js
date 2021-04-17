8ece9dd13a5fc598743d40879267c8598e0cd711
Max Nguyen <chadwick.uxd@gmail.com>
2021-04-16 21:08:28 -0400

    app update

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,7 @@
 // from data.js
 var tableData = data;
+var tableColumns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"];
+var tableBody = d3.select("tbody");
+var searchDate = d3.select("#datetime");
+var button = d3.select("#filter-btn");
 
-// YOUR CODE HERE!
